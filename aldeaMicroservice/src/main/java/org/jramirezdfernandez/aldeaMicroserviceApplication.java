2f5d3495e17fc3099e084200a543d8dd72631ad1
Diego <deigoalejo05@gmail.com>
2025-11-22 00:05:23 -0500

    Ultimas modificaciones

--- aldeaMicroservice/src/main/java/org/jramirezdfernandez/aldeaMicroserviceApplication.java
+++ aldeaMicroservice/src/main/java/org/jramirezdfernandez/aldeaMicroserviceApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MonolitoNarutoApplication {
+public class aldeaMicroserviceApplication {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(MonolitoNarutoApplication.class, args);
+        SpringApplication.run(aldeaMicroserviceApplication.class, args);
 
 
     }
--- ninjaMicroservice/src/main/java/org/jramirezdfernandez/ninjaMicroserviceApplication.java
+++ ninjaMicroservice/src/main/java/org/jramirezdfernandez/ninjaMicroserviceApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MonolitoNarutoApplication {
+public class ninjaMicroserviceApplication {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(MonolitoNarutoApplication.class, args);
+        SpringApplication.run(ninjaMicroserviceApplication.class, args);
 
 
     }
